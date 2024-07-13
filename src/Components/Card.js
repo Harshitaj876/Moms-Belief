08ff4246b1b44000a60b42ecec43289f4c1793c3
Harshitaj876 <71326956+Harshitaj876@users.noreply.github.com>
2024-07-13 19:23:46 +0530

    modified:   src/Components/Card.js 	modified:   src/Pages/Home.js

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,17 +1,22 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Card = ({data}) => {
+const Card = ({ data }) => {
   return (
     <div className='flex flex-col items-center gap-7'>
-        <img src={data.image} alt='Images' className='border-[1.5px] border-[#EA5858] p-4'></img>
-        <div className='flex flex-col gap-2'>
-            <h3 className='h-[27px] w-[231px] text-[#151B4A] font-semibold text-[20px] leading-[27px]'>{data.heading}</h3>
-            <h4 className='h-[81px] w-[290px] text-[#4E516C] font-normal text-[16px] leading-[27px]'>{data.description}</h4>
-            <Link to='/programs' className='h-[27px] w-[139px] text-[#EA5858] font-normal text-[16px] leading-[27px]'>See all programs</Link>
-        </div>
+      <img src={data.image} alt='Images' className='border-[1.5px] border-[#EA5858] p-4'></img>
+      <div className='flex flex-col gap-2 items-center sm:items-start'>
+        <h3 className='h-[27px] w-[231px] text-[#151B4A] font-semibold text-[20px] leading-[27px] text-center sm:text-left'>{data.heading}</h3>
+        <h4 className='h-[81px] w-[290px] text-[#4E516C] font-normal text-[16px] leading-[27px] text-center sm:text-left'>{data.description}</h4>
+        <Link 
+          to='/programs' 
+          className='relative h-[27px]  text-[#EA5858] font-normal text-[16px] leading-[27px] text-center sm:text-left group'>
+          See all programs
+          <span className='absolute left-0 bottom-1 w-full h-[1px] bg-[#EA5858] opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100'></span>
+        </Link>
+      </div>
     </div>
   )
 }
 
-export default Card
+export default Card;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -11,20 +11,22 @@ const Home = () => {
   ];
 
   return (
-      <div>
+      <div className='flex flex-col gap-[100px]'>
+      {/*Hero Section*/}
         <div className='flex flex-col items-center justify-center gap-7 h-[759px] bg-[#FFF6CC]'>
           <img src='/Assets/illustration1.svg' alt='' className='hidden sm:block absolute top-32 left-[300px]'></img>
           <img src='/Assets/illustration2.svg' alt='' className='hidden sm:block absolute top-32 right-[300px]'></img>
           <h1 className='font-Noto font-medium text-[25px] sm:text-[40px] leading-[35px] sm:leading-[60px]  h-[105px] w-[270px] sm:h-[120px] sm:w-[608px] text-center text-[#151B4A]'>We Empower Mom's Beliefs & Enable Childs's <span className='relative inline-block'>Growth.<img src='/Assets/underline.svg' alt='' className='absolute bottom-[-5px] left-0 right-0 w-full h-auto'></img></span></h1>
-          <h2 className='font-normal text-[18px] sm:text-[16px] leading-[25px] sm:leading-[30px] text-[#4E516C] h-[75px] w-[324px] sm:h-[60px] sm:w-[578px] text-center m-0'>We are a research-driven holistic care provider for growing children through a wide array of programs.</h2>
+          <h2 className='font-normal text-[18px] sm:text-[16px] leading-[25px] sm:leading-[30px] text-[#4E516C] h-[75px] w-[324px] sm:h-[60px] sm:w-[578px] text-center m-0 tracking-wide'>We are a research-driven holistic care provider for growing children through a wide array of programs.</h2>
           <Link to='/signup' className='font-semibold text-[15px] sm:text-[16px] bg-[#125E50] rounded-[60px] px-[15px] py-[6px] sm:px-[30px] sm:py-[10px] text-[#FFF] hover:bg-[#187767]'>Explore Programs</Link>
           <h2 className='font-normal text-[14px] sm:text-[16px] leading-[23px] sm:leading-[26px] text-[#4E516C] h-[69px] w-[254px] sm:h-[54px] sm:w-[373px] text-center'>Or Just take <a href='/assessment' className="underline">take a free assessment</a> to see how is the growth of your child.</h2>
         </div>
 
-        <div className='flex flex-col gap-[45px] items-center justify-center bg-[#E4F7F3]'>
+      {/*Programs*/}
+        <div className='flex flex-col gap-[45px] items-center justify-center bg-[#E4F7F3] h-[944px]'>
           <div className='flex flex-col gap-[15px] items-center justify-center'>
             <h1 className='font-Noto font-medium text-[20px] sm:text-[32px] leading-[33px] sm:leading-[55px]  h-[66px] w-[293.11px] sm:h-[58.28px] sm:w-[633px] text-center text-[#151B4A]'>Customisable Expert <span className='relative inline-block'>Curated<img src='/Assets/underline.svg' alt='' className='absolute bottom-[-5px] left-0 right-0 w-full h-auto'></img></span> Programs</h1>
-            <h2 className='hidden sm:block font-normal text-[18px] leading-[27px] text-[#4E516C] h-[54px] w-[766px] text-center m-0 '>These programs work on applied neurological science. Our experts have curated each program with the knowledge and insights from decades of experience.</h2>
+            <h2 className='hidden sm:block font-normal text-[18px] leading-[27px] text-[#4E516C] h-[54px] w-[766px] text-center m-0 tracking-wide'>These programs work on applied neurological science. Our experts have curated each program with the knowledge and insights from decades of experience.</h2>
           </div>
           <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-[50px] sm:gap-[100px]'>
             {data.map((item, index) => (
@@ -37,6 +39,61 @@ const Home = () => {
             ))}
           </div>
         </div>
+
+      {/*How do we work*/}
+        <div className='flex flex-col items-center justify-center'>
+            <div className='flex flex-col items-center justify-center gap-[15px]'>
+              <h1 className='font-Noto font-medium text-[20px] sm:text-[32px] leading-[50px] w-[136px] h-[33px] sm:w-[309px] sm:h-[50px] text-center'>
+                <span className='block sm:hidden'>Who are we?</span>
+                <span className='hidden sm:block'>How do we work?</span>
+              </h1>
+              <h2 className='font-normal text-base sm:text-lg sm:leading-[25px] leading-[27px] w-[303px] h-[125px] sm:w-[705px] sm:h-[54px] text-center text-[#4E516C]'> Mom’s Belief is an innovation and research-driven holistic care provider for children with Special Needs and operates through a Home-Based Program.</h2>
+            </div>
+        </div>
+
+      {/*Expert*/}
+      <div className='flex flex-col bg-[#E4F7F3] h-[817px] items-center justify-center gap-[60px]'>
+          <div className='flex flex-col items-center justify-center gap-[15px]'>
+            <h1 className='h-[110px] w-[334px] font-Noto text-center font-medium leading-[55px] text-[32px]'>Got More Questions?Talk To Our Experts</h1>
+            <h2 className='h-[60px] w-[634px] text-center font-normal leading-[30px] text-[18px] text-[#4E516C] tracking-wide'>These programs work on applied neurological science. Our experts have curated each program with the knowledge </h2>
+          </div>
+          <div className='w-[1023px] h-[452px] rounded-[14px] bg-[#FFFFFF] flex flex-col items-center justify-center gap-[50px]'>
+              <h1 className='font-semibold text-xl leading-[27px] tracking-wide h-[27px] w-[374px] text-[#151B4A]'>Find the best program for your child</h1>
+              <form className='flex flex-col'>
+                <div className='grid grid-cols-3 gap-5'>
+                  <div className='flex flex-col'>
+                    <label className='h-[17px] font-semibold text-[14px] leading-[16.94px] tracking-wide text-[#4E516C] mb-[10px]'>Your Name</label>
+                    <input placeholder='Enter your name' className='h-[45px] w-[301px] rounded-[5px] border-[1px] border-[#CAC5C5] pl-[10px]' />
+                  </div>
+                  <div className='flex flex-col'>
+                    <label className='h-[17px] font-semibold text-[14px] leading-[16.94px] tracking-wide text-[#4E516C] mb-[10px]'>Your Child's Name</label>
+                    <input placeholder="Enter your Child's name" className='h-[45px] w-[301px] rounded-[5px] border-[1px] border-[#CAC5C5] pl-[10px]' />
+                  </div>
+                  <div className='flex flex-col'>
+                    <label className='h-[17px] font-semibold text-[14px] leading-[16.94px] tracking-wide text-[#4E516C] mb-[10px]'>Child's Age</label>
+                    <input placeholder="Enter Child's age" className='h-[45px] w-[301px] rounded-[5px] border-[1px] border-[#CAC5C5] pl-[10px]' />
+                  </div>
+                </div>
+
+                <div className='flex mt-[22px] gap-5  '>
+                  <div className='flex flex-col'>
+                    <label className='h-[17px] font-semibold text-[14px] leading-[16.94px] tracking-wide text-[#4E516C] mb-[10px]'>E-mail id</label>
+                    <input placeholder='Enter email id' className='h-[45px] w-[622px] rounded-[5px] border-[1px] border-[#CAC5C5] pl-[10px]' />
+                  </div>
+                  <div className='flex flex-col'>
+                    <label className='h-[17px] font-semibold text-[14px] leading-[16.94px] tracking-wide text-[#4E516C] mb-[10px]'>Mobile Number</label>
+                    <input placeholder='Enter mobile no.' className='h-[45px] w-[301px] rounded-[5px] border-[1px] border-[#CAC5C5] pl-[10px]' />
+                  </div>
+                </div>
+              </form>
+              <div className='flex flex-col gap-[10px] items-center'>
+                <Link to='/signup' className='font-semibold text-[15px] sm:text-[16px] bg-[#125E50] rounded-[60px] px-[15px] py-[6px] sm:px-[30px] sm:py-[10px] text-[#FFF] hover:bg-[#187767] w-[174px] h-[44px]'>Request a call</Link>
+                <h3 className='w-[307px] h-[24px] font-normal text-[14px] leading-6 text-[#4E516C]'>We’ll keep your information private & secure</h3>
+              </div>
+          </div>
+      </div>
+      {/*Plans and Packages*/}
+      {/*Footer*/}
       </div>
   );
 }
