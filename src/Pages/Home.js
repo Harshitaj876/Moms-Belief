38893d4c1449d9f7adf4e6ce29ec946d95391d51
Harshitaj876 <71326956+Harshitaj876@users.noreply.github.com>
2024-07-13 16:18:37 +0530

    modified:   src/Pages/Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -21,7 +21,7 @@ const Home = () => {
           <h2 className='font-normal text-[14px] sm:text-[16px] leading-[23px] sm:leading-[26px] text-[#4E516C] h-[69px] w-[254px] sm:h-[54px] sm:w-[373px] text-center'>Or Just take <a href='/assessment' className="underline">take a free assessment</a> to see how is the growth of your child.</h2>
         </div>
 
-        <div className='flex flex-col gap-[45px] items-center justify-center'>
+        <div className='flex flex-col gap-[45px] items-center justify-center bg-[#E4F7F3]'>
           <div className='flex flex-col gap-[15px] items-center justify-center'>
             <h1 className='font-Noto font-medium text-[20px] sm:text-[32px] leading-[33px] sm:leading-[55px]  h-[66px] w-[293.11px] sm:h-[58.28px] sm:w-[633px] text-center text-[#151B4A]'>Customisable Expert <span className='relative inline-block'>Curated<img src='/Assets/underline.svg' alt='' className='absolute bottom-[-5px] left-0 right-0 w-full h-auto'></img></span> Programs</h1>
             <h2 className='hidden sm:block font-normal text-[18px] leading-[27px] text-[#4E516C] h-[54px] w-[766px] text-center m-0 '>These programs work on applied neurological science. Our experts have curated each program with the knowledge and insights from decades of experience.</h2>
